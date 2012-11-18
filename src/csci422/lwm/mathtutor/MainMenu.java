8364087132a97d8921effe9a2673305a331adbeb
Riley Moses <victi.resurgo@gmail.com>
2012-11-18 14:46:38 -0700

    Set main menu as default screen, launch game using button

--- src/csci422/lwm/mathtutor/MainMenu.java
+++ src/csci422/lwm/mathtutor/MainMenu.java
@@ -1,8 +1,23 @@
 package csci422.lwm.mathtutor;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends Activity
 {
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+	}
+	
+	public void startGame(View v)
+	{
+		Intent i = new Intent(this, MathTutor.class);
+		startActivity(i);
+	}
 
 }
